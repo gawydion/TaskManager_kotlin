a211a0463c9190f19dff779b2de473edfe5d9fda
Marcin <gawydion@gmail.com>
2017-06-25 21:49:25 +0200

    on click nie działa

--- app/src/main/java/pl/edu/pwr/kotlin/mytasks/TaskListActivity.kt
+++ app/src/main/java/pl/edu/pwr/kotlin/mytasks/TaskListActivity.kt
@@ -1,14 +1,24 @@
 package pl.edu.pwr.kotlin.mytasks
 
+import android.content.ClipData
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
+import android.view.*
 import com.mcxiaoke.koi.log.logv
 import kotlinx.android.synthetic.main.activity_task_list.*
 import pl.edu.pwr.kotlin.mytasks.adapters.TasksAdapter
 import pl.edu.pwr.kotlin.mytasks.data.Task
 import pl.edu.pwr.kotlin.mytasks.model.TasksProvider
+import android.view.ContextMenu.ContextMenuInfo
+import android.widget.Toast
+import com.mcxiaoke.koi.ext.find
+import com.mcxiaoke.koi.ext.onClick
+import com.mcxiaoke.koi.ext.toast
+import kotlinx.android.synthetic.main.activity_task_list.*
+
+
 
 class TaskListActivity : AppCompatActivity() {
 
@@ -20,8 +30,23 @@ class TaskListActivity : AppCompatActivity() {
         initYourTODOs()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+
+        var addTaskButton: View? = findViewById(R.id.main_button_addTask)
+
+        addTaskButton?.onClick {
+            Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show()
+        }
+
+
+
+    }
+
     private fun initYourTODOs() {
-        TasksProvider.add(Task(name = "build me!"))
+        //TasksProvider.add(Task(name = "build me!"))
         TasksProvider.add(Task(name = "implement add new task"))
         TasksProvider.add(Task(name = "show task detail"))
         TasksProvider.add(Task(name = "add task status"))
@@ -38,7 +63,14 @@ class TaskListActivity : AppCompatActivity() {
         tasksTasksList.adapter = TasksAdapter(this, TasksProvider.tasks)
     }
 
+    fun addTask(){
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show()
+    }
+
+
+
 
 }
+
 
 
